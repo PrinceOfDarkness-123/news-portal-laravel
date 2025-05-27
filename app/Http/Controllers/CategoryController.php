b8d3b9c64a08fd603801feb42a3fe498f494aa14
PrinceOfDarkness-123 <94347356+PrinceOfDarkness-123@users.noreply.github.com>
2025-05-27 15:35:42 +0500

    Set redirection to 404.blade.php if news not found for specific category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,12 +9,17 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function show($category_id) {
-        $newsByCategory = News::with('category')->where('category_id', $category_id)->latest()->paginate(5);
-        $category_name = $newsByCategory->first()->category->name;
+        $category = Category::findOrFail($category_id); // This will return 404 if not found
+        $category_name = $category->name;
+        $newsByCategory = News::with('category', 'author')->where('category_id', $category_id)->latest()->paginate(5);
         $activeMenuForCategory = "category_$category_id";
         //Get Categories list for navbar
         $firstFourCategories = Category::take(4)->get();
         $remainingCategories = Category::skip(4)->take(PHP_INT_MAX)->get();
-        return view('category.index', compact('newsByCategory', 'activeMenuForCategory', 'category_name', 'firstFourCategories', 'remainingCategories'));
+        if ($newsByCategory->isEmpty()) {
+            return view('category.404', compact('activeMenuForCategory', 'category_name', 'firstFourCategories', 'remainingCategories'));
+        } else {
+            return view('category.index', compact('newsByCategory', 'activeMenuForCategory', 'category_name', 'firstFourCategories', 'remainingCategories'));
+        }
     }
 }
